871dad1243a9d317508ec87c2b570a6ff67303e6
アイロム <165378124+ir0m@users.noreply.github.com>
2024-12-24 03:39:08 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAunBXed3tUraz5PQXpN1CWKpA7gTtU7YM",
-  authDomain: "twitter-clone-6d5db.firebaseapp.com",
-  projectId: "twitter-clone-6d5db",
-  storageBucket: "twitter-clone-6d5db.appspot.com",
-  messagingSenderId: "688634272264",
-  appId: "1:688634272264:web:8ccb69be6e18efafa04fbd"
+
 };
 
 
